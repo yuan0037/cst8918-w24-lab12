cf036be43dcbfc23639a117eb24c7161e300a89a
Bo Yuan <boyuxa@syntronic.com>
2024-03-30 16:05:33 -0400

    formatted tf scripts

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,12 +2,12 @@
 resource "azurerm_resource_group" "rg" {
   name     = "yuan0037-a12-rg"
   location = "canadacentral"
-      provisioner "local-exec" {
-        # command = "export subscriptionId=$(az account show --query id -o tsv)"
-        command = <<-EOT
+  provisioner "local-exec" {
+    # command = "export subscriptionId=$(az account show --query id -o tsv)"
+    command = <<-EOT
                 export subscriptionId=$(az account show --query id -o tsv)
                 export tenantId=$(az account show --query tenantId -o tsv)
                 export resourceGroupName=$(terraform output -raw resource_group_name)
         EOT
-    }
+  }
 }
