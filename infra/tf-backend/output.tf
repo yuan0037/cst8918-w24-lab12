9db71bd5d3cea75caff9c6961fc06da4c8ff4f71
Bo Yuan <boyuxa@syntronic.com>
2024-03-30 17:12:07 -0400

    removed tslint error

--- infra/tf-backend/output.tf
+++ infra/tf-backend/output.tf
@@ -1,16 +1,20 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "resource group name"
+  value       = azurerm_resource_group.rg.name
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.backendstate.name
+  description = "storage account for state file"
+  value       = azurerm_storage_account.backendstate.name
 }
 
 output "container_name" {
-  value = azurerm_storage_container.backendstate.name
+  description = "container name for state file"
+  value       = azurerm_storage_container.backendstate.name
 }
 
 output "primary_access_key" {
-  value     = azurerm_storage_account.backendstate.primary_access_key
-  sensitive = true
+  description = "primary access key for storage account"
+  value       = azurerm_storage_account.backendstate.primary_access_key
+  sensitive   = true
 }
