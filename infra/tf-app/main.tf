dbc26f0f6377e91b80101ecbc250ea54c188cd84
Bo Yuan <boyuxa@syntronic.com>
2024-03-30 21:38:50 -0400

    updated app script to create vnet and subnet.

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -11,3 +11,18 @@ resource "azurerm_resource_group" "rg" {
         EOT
     }
 }
+
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
